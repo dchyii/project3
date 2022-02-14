6cb5fba44462cc452ec4fbaa1c72c4258e959986
edichen123 <edichenjl@outlook.com>
2022-02-14 15:44:22 +0800

    added stuffs

--- client/src/Components/Photos/Photos.jsx
+++ client/src/Components/Photos/Photos.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Navbar from "../Subcomponents/Navbar";
+import SearchBar from "../Subcomponents/SearchBar";
 
 const Photos = () => {
-  return <div>Photos</div>;
+  return (
+    <div>
+      <Navbar />
+      <SearchBar />
+    </div>
+  );
 };
 
 export default Photos;
