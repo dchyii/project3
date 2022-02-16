81b1495e631aec66c1cf0cabb78bfbced094f27e
DementedDarsh <darsh.chong@agilelab.sg>
2022-02-16 20:00:23 +0800

    pre-merge

--- client/src/Components/Pages/Photos/PhotoView.jsx
+++ client/src/Components/Pages/Photos/PhotoView.jsx
@@ -3,22 +3,25 @@ import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 
 const PhotoView = () => {
-  const { postID } = useParams();
-  const [viewedPost, setViewedPost] = useState({})
+  const { postID, userID } = useParams();
+  const [viewedPost, setViewedPost] = useState({});
   useEffect(() => {
     const getPost = async () => {
       const post = await axios.get(`/api/images/${postID}`);
       console.log(post.data.data.imagePosts);
       setViewedPost(post.data.data.imagePosts);
     };
+    const getComments = async () => {
+        const post = await axios.get(`/api/images/${postID}`);
+        console.log(post);
+      };
     getPost();
   }, []);
 
   return (
     <div>
       <div>PhotoView</div>
-      <span><img src={viewedPost?.imgPath} style={{display: "inline-block", width: "50%", height: "auto"}}/></span>
-      <span>COMMENTS</span>
+      <span><img src={viewedPost.imgPath} className="w-6/12 inline"/></span><span className="w-6/12 inline">Comments</span>
     </div>
   );
 };
