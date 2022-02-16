f9314f95d9203785a0c3533fafb21183ec3ef794
DementedDarsh <darsh.chong@agilelab.sg>
2022-02-17 01:17:41 +0800

    layout of view page nearly done

--- client/src/Components/Pages/Photos/PhotoView.jsx
+++ client/src/Components/Pages/Photos/PhotoView.jsx
@@ -20,20 +20,41 @@ const PhotoView = () => {
 
   const dummyComments = ["sucks", "goood", "bad"];
   const allComments = dummyComments.map((item) => (
-    
-    <div>
-      <div>
-        <span>razzle_dazzle</span> {item}
+    <div class="mb-2">
+      <div class="mb-2 text-sm">
+        <span class="font-medium mr-2">razzle_dazzle</span> {item}
       </div>
     </div>
   ));
 
   return (
-    <div className="flex flex-row m-10"> 
+    <div className="flex flex-row m-10 rounded border">
       <span className="w-6/12">
-        <img src={viewedPost.imgPath}/>
+        <div class="w-full flex justify-between p-3">
+          <div class="flex">
+            <div class="rounded-full h-8 w-8 bg-gray-500 flex items-center justify-center overflow-hidden">
+              <img
+                src="https://avatars0.githubusercontent.com/u/38799309?v=4"
+                alt="profilepic"
+              />
+            </div>
+            <span class="pt-1 ml-2 font-bold text-sm">braydoncoyer</span>
+          </div>
+          <span class="px-2 hover:bg-gray-300 cursor-pointer rounded">
+            <i class="fas fa-ellipsis-h pt-2 text-lg"></i>
+          </span>
+        </div>
+        <img src={viewedPost.imgPath} />
+        <div class="pt-1">
+        <div class="mb-2 text-sm">
+          <span class="font-medium mr-2">braydoncoyer</span> {viewedPost.description}
+        </div>
+      </div>
       </span>
-      <span className="w-6/12"><div>{allComments}</div></span>
+      <span className="w-6/12">
+          
+        <div>{allComments}</div>
+      </span>
     </div>
   );
 };
