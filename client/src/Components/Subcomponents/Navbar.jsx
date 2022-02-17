ada726b5d93abdc2dc8b89a713ec841971032be3
dchyii <hongyichong@gmail.com>
2022-02-18 04:13:16 +0800

    add app name

--- client/src/Components/Subcomponents/Navbar.jsx
+++ client/src/Components/Subcomponents/Navbar.jsx
@@ -73,7 +73,31 @@ function Navbar() {
       <nav>
         <div className="w-full md:w-10/12 h-9 mx-auto px-5 text-slate-100 flex justify-between items-center">
           <div id="logo" className="text-xl">
-            <Link to="/">Logo</Link>
+            <Link to="/">
+              <div className="flex">
+                <p>OTOF</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-blue-300"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+              </div>
+            </Link>
           </div>
           <NavbarButtons />
         </div>
