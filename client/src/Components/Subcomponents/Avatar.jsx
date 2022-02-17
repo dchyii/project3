8ccfc3dea3f8bf0bcf9273ebd4a0bf75e6a51b40
dchyii <hongyichong@gmail.com>
2022-02-18 03:16:59 +0800

    updated avatar css

--- client/src/Components/Subcomponents/Avatar.jsx
+++ client/src/Components/Subcomponents/Avatar.jsx
@@ -5,23 +5,25 @@ const Avatar = (props) => {
   // console.log(props?.user?.profilePhoto);
   return (
     <div className={`w-96 ${props.height} border m-5 py-5 rounded-lg`}>
-      <div className="px-10">
-        <img
-          alt={""}
-          // className={props?.user?.profilePhoto ? "" : " hidden"}
-          src={
-            props?.user?.profilePhoto
-              ? props?.user?.profilePhoto
-              : "https://icon-library.com/images/default-user-icon/default-user-icon-13.jpg"
-          }
-          className="shadow-lg rounded-full mx-auto bg-cover"
-        />
-        <div className="pt-6 text-center">
-          <h5 className="text-xl font-bold text-blueGray-700">
-            {props?.user?.username}
-          </h5>
+      <a href={`/${props?.user?.username}/posts`}>
+        <div className="px-10">
+          <img
+            alt={""}
+            // className={props?.user?.profilePhoto ? "" : " hidden"}
+            src={
+              props?.user?.profilePhoto
+                ? props?.user?.profilePhoto
+                : "https://icon-library.com/images/default-user-icon/default-user-icon-13.jpg"
+            }
+            className="shadow-lg rounded-full mx-auto aspect-square bg-cover"
+          />
+          <div className="pt-6 text-center">
+            <h5 className="text-xl font-bold text-blueGray-700">
+              {props?.user?.username}
+            </h5>
+          </div>
         </div>
-      </div>
+      </a>
     </div>
   );
 };
