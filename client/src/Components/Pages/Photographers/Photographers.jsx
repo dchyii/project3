e32ed38be63c6f03f2453f6af41f61713a5a65e9
dchyii <hongyichong@gmail.com>
2022-02-18 03:12:50 +0800

    completed all photographers page

--- client/src/Components/Pages/Photographers/Photographers.jsx
+++ client/src/Components/Pages/Photographers/Photographers.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import Avatar from "../../Subcomponents/Avatar";
 import PhotographerCard from "../../Subcomponents/PhotographerCard";
 
 const Photographers = (props) => {
+  console.log(props.users);
+  const allUsers = props.users;
+
+  const mappedUsers = allUsers.map((user, index) => {
+    return <Avatar user={user} key={index} height={"h-96"} />;
+  });
+
   return (
-    <div>
-      <PhotographerCard photo={props.photos} />
-    </div>
+    <div className="bg-slate-100 w-full flex flex-wrap">{mappedUsers}</div>
   );
 };
 
